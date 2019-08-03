7dbe8cbdcb001b815b30768c219d6f6a6e89a9b4
pythagoras <magicianhy@126.com>
2019-08-04 00:37:14 +0800

    change file

--- src/main/java/org/py/web/view/freemarker/FreemarkerTemplate.java
+++ src/main/java/org/py/web/view/freemarker/FreemarkerTemplate.java
@@ -1,4 +1,20 @@
 package org.py.web.view.freemarker;
 
-public class FreemarkerTemplate {
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+import java.io.Writer;
+
+public final class FreemarkerTemplate {
+    private final static Configuration cfg;
+    private FreemarkerTemplate() { }
+    static {
+        cfg = new Configuration(Configuration.VERSION_2_3_28);
+    }
+    public static synchronized void process(final String templateName, final Writer out, Object model) throws IOException, TemplateException {
+        Template template = cfg.getTemplate(templateName);
+        template.process(model, out);
+    }
 }
