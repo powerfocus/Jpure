c28a458e038151d8ca73e9707b115fe2a15a5b6e
pythagoras <magicianhy@126.com>
2019-08-06 23:48:53 +0800

    change

--- src/main/java/org/py/jpure/core/reflect/ReflectUtils.java
+++ src/main/java/org/py/jpure/core/reflect/ReflectUtils.java
@@ -1,4 +1,20 @@
 package org.py.jpure.core.reflect;
 
-public class ReflectUtils {
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public final class ReflectUtils {
+    private ReflectUtils() { }
+    public static Class<?> forName(String className) throws ClassNotFoundException {
+        return Class.forName(className);
+    }
+    public static Object newInstance(Class<?> clazz) throws IllegalAccessException, InstantiationException {
+        return clazz.newInstance();
+    }
+    public static Object invoke(Object obj, Method method, Object... args) throws InvocationTargetException, IllegalAccessException {
+        return method.invoke(obj, args);
+    }
+    public static Method getMethod(Class<?> clazz, String methodName, Class<?>... args) throws NoSuchMethodException {
+        return clazz.getMethod(methodName, args);
+    }
 }
--- src/main/test/java/org/py/jpure/core/reflect/ReflectTest.java
+++ src/main/test/java/org/py/jpure/core/reflect/ReflectTest.java
@@ -1,6 +1,7 @@
 package org.py.jpure.core.reflect;
 
 import org.junit.Test;
+import org.py.jpure.support.ModelMap;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -10,12 +11,16 @@ public class ReflectTest {
     public void test() throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         Class<?> aClass = Class.forName("org.py.arena.controller.IndexController");
         Object obj = aClass.newInstance();
-        Method index = aClass.getMethod("index");
-        final Object invoke = index.invoke(obj);
+        Method index = aClass.getMethod("index", ModelMap.class);
+        final Object invoke = index.invoke(obj, new ModelMap());
         System.out.println(invoke);
     }
     @Test
-    public void test2() {
-
+    public void test2() throws Exception {
+        Class<?> aClass = ReflectUtils.forName("org.py.arena.controller.IndexController");
+        Object obj = ReflectUtils.newInstance(aClass);
+        Method index = ReflectUtils.getMethod(aClass, "index", ModelMap.class);
+        Object invoke = ReflectUtils.invoke(obj, index, new ModelMap());
+        System.out.println(invoke);
     }
 }
