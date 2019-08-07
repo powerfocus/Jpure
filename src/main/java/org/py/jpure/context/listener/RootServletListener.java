ab5661c0994dd4c2d0f70cba414222555d49344e
pythagoras <magicianhy@126.com>
2019-08-07 18:09:31 +0800

    change

--- src/main/java/org/py/jpure/context/listener/RootServletListener.java
+++ src/main/java/org/py/jpure/context/listener/RootServletListener.java
@@ -1,26 +1,15 @@
 package org.py.jpure.context.listener;
 
-import org.py.jpure.context.WebRootResolver;
-import org.py.jpure.context.filter.CharacterEncodingFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
 
-@WebListener
 public class RootServletListener implements ServletContextListener {
     private static final Logger log = LoggerFactory.getLogger(RootServletListener.class);
-    private static final WebRootResolver webRootResolver;
-    static {
-        webRootResolver = new WebRootResolver();
-    }
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        webRootResolver.servletContext(sce.getServletContext());
-        webRootResolver.addFilter("characterEncodingFilter", CharacterEncodingFilter.class, "/*");
-        webRootResolver.register();
         log.info(getClass().getName() + " initialized. web starting up...");
     }
 
--- src/main/java/org/py/jpure/web/JPureServletInitializer.java
+++ src/main/java/org/py/jpure/web/JPureServletInitializer.java
@@ -1,5 +1,8 @@
 package org.py.jpure.web;
 
+import org.py.jpure.context.WebRootResolver;
+import org.py.jpure.context.filter.CharacterEncodingFilter;
+import org.py.jpure.context.listener.RootServletListener;
 import org.py.jpure.context.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContainerInitializer;
@@ -11,8 +14,16 @@ import java.util.Set;
 
 @HandlesTypes(WebApplicationInitializer.class)
 public class JPureServletInitializer implements ServletContainerInitializer {
+    private static final WebRootResolver webRootResolver;
+    static {
+        webRootResolver = new WebRootResolver();
+    }
     @Override
     public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
+        servletContext.addListener(RootServletListener.class);
+        webRootResolver.servletContext(servletContext);
+        webRootResolver.addFilter("characterEncodingFilter", CharacterEncodingFilter.class, "/*");
+        webRootResolver.register();
         ServletRegistration.Dynamic dispatcherServlet = servletContext.addServlet("dispatcherServlet", DispatcherServlet.class);
         dispatcherServlet.setLoadOnStartup(1);
         dispatcherServlet.addMapping("/");
